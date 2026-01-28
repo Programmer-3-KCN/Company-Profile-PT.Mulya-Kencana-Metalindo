33bccdf802f00437c547f537ef97b96ea9aa8b04
Programmer-3-KCN <programmer3.it@kcngroup.co.id>
2026-01-28 13:56:41 +0700

    refactor: 💡 change google maps iframe address

--- src/layouts/home/modules/main/batches/content/index.tsx
+++ src/layouts/home/modules/main/batches/content/index.tsx
@@ -988,15 +988,12 @@ const ContactSection: FC = (): ReactElement => {
             transition={{ delay: 0.3, duration: 0.6 }}
           >
             <iframe
-              allowFullScreen
-              className="w-full"
-              height="400"
+              allowFullScreen={true}
+              className="size-full border-0"
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.7985!2d107.5766!3d-6.9175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwNTUnMDMuMCJTIDEwN8KwMzQnMzUuOCJF!5e0!3m2!1sen!2sid!4v1234567890"
-              style={{ border: 0 }}
-              title="Lokasi PT Mulya Kencana Metalindo"
-            />
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.6962659152127!2d107.59719319999999!3d-6.9268629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e61f857b6e79%3A0x84033ec28c966976!2sPT.%20Mulya%20Kencana%20Metalindo!5e0!3m2!1sen!2sid!4v1769582899158!5m2!1sen!2sid"
+            ></iframe>
           </motion.div>
         </div>
       </div>
