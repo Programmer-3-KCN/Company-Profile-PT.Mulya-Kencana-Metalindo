df2b865a83b892e49ac08a4baaaefce358493125
Programmer-3-KCN <programmer3.it@kcngroup.co.id>
2026-01-28 13:31:49 +0700

    feat: 🎉 initial design

--- src/layouts/home/modules/main/index.tsx
+++ src/layouts/home/modules/main/index.tsx
@@ -1,5 +1,12 @@
 import { FC, PropsWithChildren, ReactElement } from "react";
 
+import { Content } from "./batches";
+
 type T = Readonly<PropsWithChildren>;
 
-export const Main: FC<T> = (props): ReactElement => <main>{props.children}</main>;
+export const Main: FC<T> = (props): ReactElement => (
+  <main>
+    <Content />
+    {props.children}
+  </main>
+);
